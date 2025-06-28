00a1940912b40101b094ad3c365b74c12f6450ef
吟风抱月 <3472151649@qq.com>
2025-06-28 22:26:40 +0800

    初始化

---
+++ src/main/java/com/yygx/springaibot/SpringaiBotApplication.java
@@ -0,0 +1,13 @@
+package com.yygx.springaibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yygx/springaibot/SpringaiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yygx.springaibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
