7ee1d1a095b00b622dc52d23fb8d9bfcdfbf6a87
吟风抱月 <3472151649@qq.com>
2025-06-29 00:41:44 +0800

    SimpleLoggerAdvisor

--- src/main/java/com/yygx/springaibot/config/CommonConfiguration.java
+++ src/main/java/com/yygx/springaibot/config/CommonConfiguration.java
@@ -1,6 +1,7 @@
 package com.yygx.springaibot.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ public class CommonConfiguration {
         return ChatClient
                 .builder(chatModel)
                 .defaultSystem("你是一个资深Java后端开发专家，你叫张三，请用资深后端开发专家张三的身份回答相关问题")
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor()
+                )
                 .build();
     }
 }
